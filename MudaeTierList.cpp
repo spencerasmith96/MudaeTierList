a89efee4fc19b5ba3ead2633b3dce1ad6653bcfc
Spencer <spencerasmith96@gmail.com>
2021-07-16 00:11:19 -0400

    save/load data from unassigned characters

--- MudaeTierList.cpp
+++ MudaeTierList.cpp
@@ -1,15 +1,208 @@
 #include "mudaetierlist.h"
-#include "ui_mudaetierlist.h"
+#include "ui_MudaeTierList.h"
+#include "characterwidget.h"
+#include "characterdata.h"
+#include "addcharactersdialog.h"
+#include <iostream>
+#include <QWidget>
+#include <QFileDialog>
+#include <QMessageBox>
 
 MudaeTierList::MudaeTierList(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MudaeTierList)
 {
     ui->setupUi(this);
+
+    if(!QSqlDatabase::drivers().contains("QSQLITE")){
+        std::cout << "install driver plz" << std::endl;
+    }
+
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    QString dbPath = QCoreApplication::applicationDirPath() + "/Mudae.db";
+    std::cout << dbPath.toStdString() << std::endl;
+    db.setDatabaseName(dbPath);
+    db.open();
+    setAcceptDrops(true);
+
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(onButtonClicked()));
+    connect(ui->actionNew, &QAction::triggered, this, &MudaeTierList::onNewTierList);
+    connect(ui->actionSaveAs, &QAction::triggered, this, &MudaeTierList::onSaveAs);
+    connect(ui->actionAddCharacters, &QAction::triggered, this, &MudaeTierList::onAddCharacters);
+    connect(ui->actionOpen, &QAction::triggered, this, &MudaeTierList::onOpen);
 }
 
 MudaeTierList::~MudaeTierList()
 {
+    db.close();
+    clearData();
     delete ui;
+}
+
+TierWidget* MudaeTierList::addTier(QString tierLabel, QString backgroundColor)
+{
+    TierWidget *newTier = new TierWidget(tierLabel, backgroundColor);
+    tierWidgets.append(newTier);
+    ui->tiersLayout->addWidget(newTier);
+    return newTier;
+}
+
+void MudaeTierList::onButtonClicked()
+{
+    QString bestGirl = "Hifumin";
+    addCharacter(bestGirl);
+}
+
+void MudaeTierList::onNewTierList()
+{
+    clearData();
+
+    // Sets up initial tiers
+    addTier("S", "Red");
+    addTier("A", "Orange");
+    addTier("B", "Yellow");
+    addTier("C", "Green");
+    addTier("D", "Purple");
+    addTier("F", "Light Blue");
+}
+
+void MudaeTierList::onDropped(CharacterWidget *droppedCharacter)
+{
+    if(characterWidgets.contains(droppedCharacter)){
+        characterWidgets.removeOne(droppedCharacter);
+        delete droppedCharacter;
+    }
+}
+
+void MudaeTierList::onSaveAs()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save Tier List"), "NewTierList", tr("Tier List (*.tl);;All Files (*)"));
+
+    if(fileName.isEmpty()){
+        return;
+    }
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly)){
+        QMessageBox::information(this, tr("Unable to open file"), file.errorString());
+        return;
+    }
+
+    QDataStream out(&file);
+    out << tierWidgets.size();
+    for(auto tier: tierWidgets){
+        out << tier->getName() << tier->getColor() << tier->getCharacterNames();
+    }
+    QStringList unassignedCharacters;
+    for(auto character: characterWidgets){
+        unassignedCharacters.append(character->getName());
+    }
+    out << unassignedCharacters;
+    file.close();
+}
+
+void MudaeTierList::onAddCharacters()
+{
+    AddCharactersDialog *newDialog = new AddCharactersDialog(this);
+    newDialog->open();
+    connect(newDialog, &AddCharactersDialog::accepted, this, &MudaeTierList::onAddCharactersAccepted);
+}
+
+void MudaeTierList::onAddCharactersAccepted(AddCharactersDialog *acceptedDialog)
+{
+    QStringList characterNames = acceptedDialog->getLines();
+    QString notFoundCharacters;
+    for(auto name: characterNames){
+        if(!addCharacter(name)){
+            notFoundCharacters.append(name + "\n");
+        }
+    }
+    acceptedDialog->deleteLater();
+    if(notFoundCharacters.length() > 0){
+        QMessageBox::warning(this, tr("Characters not found in database:"), notFoundCharacters);
+    }
+}
+
+void MudaeTierList::onOpen()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Save Tier List"), "", tr("Tier List (*.tl);;All Files (*)"));
+
+    if(fileName.isEmpty()){
+        return;
+    }
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly)){
+        QMessageBox::information(this, tr("Unable to open file"), file.errorString());
+        return;
+    }
+
+    clearData();
+    QDataStream in(&file);
+    qsizetype tierCount;
+    in >> tierCount;
+    tierWidgets.reserve(tierCount);
+    for(qsizetype tierIndex = 0; tierIndex < tierCount; ++tierIndex){
+        QString name, color;
+        QStringList characterNames;
+        in >> name >> color >> characterNames;
+        std::cout << tierIndex << name.toStdString() << std::endl;;
+        TierWidget *newTier = addTier(name, color);
+        int characterIndex = 0;
+        for(QString& character: characterNames){
+            std::cout << character.toStdString();
+            CharacterData newData(character, db);
+            if(newData.isValid()){
+                if(characterIDs.find(newData.getid()) != characterIDs.end()){
+                    continue;
+                }
+                characterIDs.emplace(newData.getid());
+                CharacterWidget* newCharacter = newData.toWidget();
+                newTier->addCharacter(newCharacter, characterIndex++);
+            }
+        }
+    }
+
+    QStringList unassignedCharacters;
+    in >> unassignedCharacters;
+    for(auto character: unassignedCharacters){
+        addCharacter(character);
+    }
+
+    file.close();
+}
+
+void MudaeTierList::clearData()
+{
+    // Clears characters
+    for(auto character: characterWidgets){
+        delete character;
+    }
+    characterWidgets.clear();
+
+    // Clears tiers
+    for(auto tier: tierWidgets){
+        delete tier;
+    }
+    tierWidgets.clear();
+
+    characterIDs.clear();
+}
+
+bool MudaeTierList::addCharacter(QString &characterName)
+{
+    CharacterData newData(characterName, db);
+    if(newData.isValid()){
+        if(characterIDs.find(newData.getid()) != characterIDs.end()){
+            return true;
+        }
+        characterIDs.emplace(newData.getid());
+        CharacterWidget* newCharacter = newData.toWidget();
+        ui->unassignedCharactersLayout->addWidget(newCharacter);
+        characterWidgets.append(newCharacter);
+        connect(newCharacter, &CharacterWidget::dropped, this, &MudaeTierList::onDropped);
+        return true;
+    }
+    return false;
 }
 
--- MudaeTierList.h
+++ MudaeTierList.h
@@ -2,6 +2,14 @@
 #define MUDAETIERLIST_H
 
 #include <QMainWindow>
+#include <vector>
+#include <QLabel>
+#include "characterwidget.h"
+#include "tierwidget.h"
+#include "addcharactersdialog.h"
+#include <QtSql>
+#include <QDropEvent>
+#include <unordered_set>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MudaeTierList; }
@@ -10,12 +18,31 @@ QT_END_NAMESPACE
 class MudaeTierList : public QMainWindow
 {
     Q_OBJECT
+private:
+    Ui::MudaeTierList *ui;
+    QVector<CharacterWidget*> characterWidgets;
+    QVector<TierWidget*> tierWidgets;
+    QSqlDatabase db;
+    std::unordered_set<int> characterIDs;
 
 public:
     MudaeTierList(QWidget *parent = nullptr);
     ~MudaeTierList();
 
+    TierWidget* addTier(QString tierLabel = QString(), QString backgroundColor = QString());
+
+public slots:
+    void onButtonClicked();
+    void onNewTierList();
+    void onDropped(CharacterWidget *droppedCharacter);
+    void onSaveAs();
+    void onAddCharacters();
+    void onAddCharactersAccepted(AddCharactersDialog *acceptedDialog);
+    void onOpen();
+
 private:
-    Ui::MudaeTierList *ui;
+    void clearData();
+    bool addCharacter(QString &characterName);
+
 };
 #endif // MUDAETIERLIST_H
